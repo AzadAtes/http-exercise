e2c39cdd22646c9b1f11c5150ed74ec163505856
az <az.dev@e.mail.de>
2025-04-28 10:40:25 +0200

    init

---
+++ src/main/java/com/example/httpexercise/HttpExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/httpexercise/HttpExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
